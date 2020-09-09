1a8f1201182c9f7c9ac89c6c4b117efbbc5519cc
Jatin Aggarwal <43716519+jatinagg1@users.noreply.github.com>
2020-09-09 20:29:27 +0530

    Update urls.py

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 
 urlpatterns = {
     url(r'^admin/', include(admin.site.urls)),
-    # if the URL pattern match /admin/ then open up admin panel
 
     url(r'', include('shortenersite.urls', namespace="shortenersite")),
-    # if anything rather then /admin/ then it will look for shortenersite/urls
 }
